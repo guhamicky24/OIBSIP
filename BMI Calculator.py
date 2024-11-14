19e00e3540a10059a41e16dd91c737dd5a99963b
Micky Guha <mickyguha8@gmail.com>
2024-11-14 23:09:24 +0530

    BMI Calculator.py

--- BMI Calculator.py
+++ BMI Calculator.py
@@ -1,12 +1,16 @@
 ## BMI calculater by Micky Guha...
+#Taking input from user
 h=float(input("Enter your height: "))
-W=float(input("Enter your wight: "))
-#Calculating the height in metre...
+W=float(input("Enter your weight: "))
+print(f'Height in centimetre is: {h}')
+#Calculating the height in meter...
 H=(h/100)
+print(f'Height in metre is: {H}')
 #Calculating the BMI...
 BMI=(W/(H*H))
 print(f'The BMI is: {BMI}')
 #Checking the BMI for result...
+#Printing the final result..
 if(BMI>0):
     if(BMI<=16.0):
         print("Severly UnderBMI ..")
